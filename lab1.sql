d486ef5267082d761848aef81f2dd84d7008efe6
namnguyen55p24 <115022325+namnguyen55p24@users.noreply.github.com>
2023-03-21 17:01:00 +0700

    nộp bài tập

---
+++ lab1.sql
@@ -0,0 +1,202 @@
+﻿Use master 
+Create database Quanlidean 
+USE Quanlidean
+
+Create table Phancong 
+(
+Manv varchar(9) not null  ,
+Mada varchar(2) not null ,
+Thoigian numeric(18,0)  ,
+Unique (Manv,Mada) 
+)
+dROP TABLE Dean
+Create table Dean 
+(
+Mada varchar(2) not null  ,
+Tenda nvarchar(50)  ,
+DDiemDA varchar(20)  ,
+Primary key (Mada) 
+)
+CREATE TABLE NHANVIEN
+(
+	MANV VARCHAR(9) NOT NULL,
+	TENLOT NVARCHAR(30),
+	TENNV NVARCHAR(30),
+	HONV NVARCHAR(15),
+	NGSINH smalldatetime ,
+	DCHI NVARCHAR(150),
+	PHAI NVARCHAR(3),
+	LUONG numeric(18,0),
+	PHG varchar(9) ,
+	
+	PRIMARY KEY (MANV)
+)
+Create table Phongban 
+(
+Maphg varchar(2) not null  ,
+Tenphg nvarchar(20) ,
+Primary key  (Maphg)
+)
+CREATE TABLE THANNHAN
+(
+	MANV VARCHAR(9) NOT NULL UNIQUE,
+	TENTN VARCHAR(20) Not null,
+	NGSINH smalldatetime ,
+	PHAI VARCHAR(3),
+	Quanhe varchar(15),
+	
+	PRIMARY KEY (TENTN)
+)
+ALTER TABLE Phancong
+ADD CONSTRAINT FK_Phancong_Manv
+FOREIGN KEY (Manv)
+REFERENCES NHANVIEN (Manv)
+
+ALTER TABLE Phancong
+ADD CONSTRAINT FK_Phancong_Madean
+FOREIGN KEY (Mada)
+REFERENCES  Dean (Mada)
+
+ALTER TABLE THANNHAN
+ADD CONSTRAINT FK_THANNHAN_MANV
+FOREIGN KEY (MANV)
+REFERENCES NHANVIEN (MANV)
+
+
+SELECT *
+FROM Phongban 
+INSERT INTO Phongban
+VALUES(1,'Quản lý')
+INSERT INTO Phongban
+VALUES(4,'Điều hành')
+INSERT INTO Phongban
+VALUES(5,'Nghiên cứu')
+
+delete from NHANVIEN
+SELECT *
+FROM NHANVIEN
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	VALUES ('123',N'Đinh', N'Bá', N'Tiến' , '02/27/1982','MỘ ĐỨC','Nam', NULL, 4)
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('234','Nguyễn','Thanh','Tùng','08-12-1956','Sơn Tịnh','Nam',null,'5')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('345','Bùi','Thúy','Vũ',null,'Tư Nghĩa','Nữ',null,'4')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('456','Lê','Thị','Nhàn','07-12-1962','Mộ Đức','Nữ',null,'4')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('567','Nguyễn','Mạnh','Hùng','03-25-1985','Sơn Tịnh','Nam',null,'5')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('678','Trần','Hồng','Quan',null,'Bình Sơn','Nam',null,'5')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('789','Trần','Thanh','Tâm','06-17-1972','TP Quảng Ngãi','Nam',null,'5')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)
+	values('890','Cao','Thanh','Huyền',null,'Tư Nghã','Nữ',null,'1')
+INSERT INTO NHANVIEN (HONV, TENLOT, TENNV, MANV, NGSINH, DCHI, PHAI, LUONG, PHG)	
+    values('901','Vương','Ngọc','Quyền','12-12-1987','Mộ Đức','Nam',null,'1')
+
+insert into PHO
+ALTER TABLE NHANVIEN
+ADD CONSTRAINT FK_NHANVIEN_PHG
+FOREIGN KEY (PHG)
+REFERENCES Phongban (Maphg)
+
+ALTER TABLE NHANVIEN
+DROP COLUMN PHG ;
+
+ALTER TABLE NHANVIEN
+
+ADD PHG varchar(2);
+delete from NHANVIEN
+ALTER TABLE Phancong
+ADD
+CONSTRAINT PK_Phancong PRIMARY KEY (Manv,Mada)
+
+ALTER TABLE THANNHAN
+ADD
+CONSTRAINT PK_THANNHAN PRIMARY KEY (Manv)
+
+ALTER TABLE Phongban
+
+ALTER COLUMN Tenphg nvarchar(30);
+
+ALTER TABLE Dean
+
+ALTER COLUMN DDiemDA  nvarchar(20);
+
+ALTER TABLE Dean
+
+ALTER COLUMN DDiemDA  nvarchar(20);
+
+ALTER TABLE THANNHAN
+
+ALTER COLUMN TENTN  nvarchar(20);
+
+ALTER TABLE THANNHAN
+
+ALTER COLUMN PHAI NVARCHAR (3);
+
+ALTER TABLE THANNHAN
+
+ALTER COLUMN Quanhe NVARCHAR (15);
+
+
+
+
+SELECT *
+FROM DEAN
+INSERT INTO DEAN (Mada,Tenda,DDiemDa)
+values('1','Nâng cao chất lượng muối','Sa Huỳnh')
+INSERT INTO DEAN (Mada,Tenda,DDiemDa)
+values('10','Xây dựng nhà máy chế biến thủy sản','Dung Quất')
+INSERT INTO DEAN (Mada,Tenda,DDiemDa)
+values('2','Phát triển hạ tầng mạng','TP Quảng Ngãi')
+INSERT INTO DEAN (Mada,Tenda,DDiemDa)
+values('20','Truyền tải cáp quang','TP Quảng Ngãi')
+INSERT INTO DEAN (Mada,Tenda,DDiemDa)
+values('3','Tin học hóa trường học','Ba tơ')
+INSERT INTO DEAN (Mada,Tenda,DDiemDa)
+values('30','Đào tạo nhân lực','Tịnh Phong')
+
+SELECT *
+FROM Phancong
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('123','1','33')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('123','2','8')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('345','10','10')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('345','20','10')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('345','3','10')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('456','1','20')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('456','2','20')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('678','3','40')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('789','10','35')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('789','20','30')
+INSERT INTO  Phancong (MaNV,Mada,Thoigian)
+values('789','30','5')
+
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('123','Châu','10-30-2005','Nữ','Con gái')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('123','Duy','10-25-2001','Nam','Con trai')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('123','Phương','10-30-1985','Nữ','Vợ chồng')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('234','Thanh','04-05-1980','Nữ','Con gái')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('345','Dương','10-30-1956','Nam','Vợ chồng')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('345','Khang','10-25-1982','Nam','Con trai')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('456','Hùng','01-01-1987','Nam','Con trai')
+INSERT INTO THANNHAN (MaNV,TENTN,NGSINH,PHAI,Quanhe)
+values('901','Thương','04-05-1989','Nữ','Vợ chồng')
+
+
